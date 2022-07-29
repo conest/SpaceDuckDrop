a3fb8a2e039dad46ef80bcdaef00ce9a17a12fba
corvusnest <corvusnest@live.com>
2022-07-29 15:51:39 +0800

    add icon

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 #! /usr/bin/env python3
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import sys
 import pygame
 import engine
 from gamescene import gameScene
 
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    os.chdir(sys._MEIPASS)
+
 
 def main():
+
     WINDOW_SIZE = (450, 600)
     WINDOW_FLAG = pygame.RESIZABLE | pygame.SCALED
     WINDOW_CAPTION = "Space Duck Drop!"
     engine.main.init(WINDOW_SIZE, WINDOW_FLAG, WINDOW_CAPTION)
+
+    pygame_icon = pygame.image.load("assets/icon.png")
+    pygame.display.set_icon(pygame_icon)
 
     gameScene.init()
     engine.main.load_scene(gameScene)
